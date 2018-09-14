6bb6a2fc1dceae5b19f2f1d66bcdd1d8adbc6f13
colb <zhangxiao@wshifu.com>
2018-09-14 15:15:20 +0800

    fixed bug

--- server/controllers/acticles.js
+++ server/controllers/acticles.js
@@ -44,7 +44,7 @@ exports.List = async(function* (req, res) {
  * Upload an image
  */
 exports.create = async(function* (req, res) {
-  const article = new Article(only(req.body, 'title link author description time content images'));
+  const article = new Article(only(req.body, 'title link tag author description content images'));
   try {
     yield article.save();
     res.json({ status: '保存成功' })
